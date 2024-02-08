40d6d215545ba78f3cba6c67a973653aa4aecbef
ayoubleass <ayoubleas@gmail.com>
2024-02-08 02:54:20 -0800

    File : console.py which is the entry point to the program

--- console.py
+++ console.py
@@ -1,17 +1,45 @@
 #!/usr/bin/python3
-from models import storage
-from models.base_model import BaseModel
+"""
+"""
+import cmd
+from models import base_model, storage
 
 
-all_objs = storage.all()
-print("-- Reloaded objects --")
-for obj_id in all_objs.keys():
-    obj = all_objs[obj_id]
-    print(obj)
+class HBNBCommand(cmd.Cmd):
+    """
+    This class is the entry point 
+    """
 
-print("-- Create a new object --")
-my_model = BaseModel()
-my_model.name = "My_First_Model"
-my_model.my_number = 89
-my_model.save()
-print(my_model)
+    intro = ""
+    prompt =  "(hbnb) "
+
+    def do_create(self, model_name):
+        if not model_name:
+            print("** class name missing **")    
+        elif model_name not in dir(base_model):
+            print("** class doesn't exist **")
+        else:
+            my_model = getattr(base_model, model_name)()
+            my_model.save()
+            print(my_model.id)
+
+    def do_show(self, line):
+        print(storage.all())
+
+    def do_quit(self, line):
+        """
+        Quit command to exit the program
+        """
+        return True
+
+    def do_EOF(self, line):
+        """
+        exit the program
+        """
+        return True
+
+
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
